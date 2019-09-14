7c567a9c9eea4acf98f25903e5a2cc4adc143a4f
Ortheon <alaricker2@gmail.com>
2019-09-14 14:22:05 +0200

    MainMenuScreen added

---
+++ lib/screens/mainScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  static const routeName = 'main-screen';
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).canvasColor,
+      ),
+    );
+  }
+}
