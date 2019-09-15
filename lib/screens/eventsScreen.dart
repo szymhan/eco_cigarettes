2165fad3ea92923e458c61c5a442181517f9bb4c
Szymon Hanzel <szyhan@gmail.com>
2019-09-15 02:13:37 +0200

    eventScreen is now clean and running

--- lib/screens/eventsScreen.dart
+++ lib/screens/eventsScreen.dart
@@ -24,11 +24,14 @@ class _EventsScreenState extends State<EventsScreen> {
     ListTile makeListTile(Event event) => ListTile(
       contentPadding:
       EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      leading:  Container(
-          width: 190.0,
-          height: 190.0,
+      leading:  ClipRRect(
+        borderRadius: new BorderRadius.circular(10.0),
+        child: Container(
+          width: 60.0,
+          height: 60.0,
+          padding: EdgeInsets.only(right: 12.0),
           decoration: new BoxDecoration(
-              shape: BoxShape.circle,
+
               image: new DecorationImage(
                   image: AssetImage(
                       event.image),
@@ -37,44 +40,37 @@ class _EventsScreenState extends State<EventsScreen> {
 
                 ),
               )
-          ),
+          ),),
       title: Text(
         event.title,
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
       ),
       // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
       subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const Padding(padding: EdgeInsets.only(top: 15.0)),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(left: 0.0),
-              child: Text(event.date,
-                  style: TextStyle(color: Colors.white,
+          Text(event.date +'\n' + event.address,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(color: Colors.black54,
                     fontSize: 16,
                   )),
-            ),
-          ),Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(left: 0.0),
-              child: Text(event.address,
-                  style: TextStyle(color: Colors.white,
-                    fontSize: 16,
-                  )),
-            ),
-          ),
         ],
       ),
       trailing:
-      Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+      Icon(FontAwesomeIcons.facebookSquare, color: Color.fromRGBO(158, 202, 232, 1), size: 30.0),
     );
 
     Card makeCard(Event event) => Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
       elevation: 8.0,
       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
       child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        height: 110,
+        decoration: BoxDecoration(color: Color.fromRGBO(241, 242, 246, .9)),
         child: makeListTile(event),
       ),
     );
@@ -95,7 +91,14 @@ class _EventsScreenState extends State<EventsScreen> {
               ), onPressed: (){
 
               }),
-              const Padding(padding: EdgeInsets.only(top: 50.0)),
+              Text('See events',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+                fontFamily: 'FitaSans',
+              ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 20.0)),
               ListView.builder(
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
@@ -109,11 +112,7 @@ class _EventsScreenState extends State<EventsScreen> {
         ),
       ),
     );
-
-
   }
-
-
 }
 
 
@@ -128,15 +127,21 @@ List getEvents() {
     Event(
         address: 'Warszawa',
         date: '2019-10-10',
-        image: "assets/trash.png",
-        title: 'Spraying flowers with tobacco water',
-        url: 'https://www.facebook.com/events/704778479971352/'),
+        image: "assets/trash3.png",
+        title: 'Spacer połączony ze zbieraniem śmieci',
+        url: 'https://www.facebook.com/events/2686611354700383/'),
     Event(
-        address: 'Żyrardów',
+        address: 'Elbląg',
         date: '2019-10-04',
-        image: "assets/trash.png",
-        title: 'Spraying flowers with tobacco water',
-        url: 'https://www.facebook.com/events/704778479971352/'),
+        image: "assets/trash2.png",
+        title: 'Zbieranie śmieci z brzegów Polskich Wód',
+        url: 'https://www.facebook.com/events/247016369573046/'),
+    Event(
+        address: 'Kraków',
+        date: '2019-10-07',
+        image: "assets/trash4.png",
+        title: 'Plogging Kraków czyli bieganie + zbieranie śmieci z Nauka Biega',
+        url: 'https://www.facebook.com/events/431508154327298/'),
   ];
 }
 
