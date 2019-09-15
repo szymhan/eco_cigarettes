3c2dcb68ae7788ab292e1098c4680e9e1c220940
Ortheon <alaricker2@gmail.com>
2019-09-15 03:01:45 +0200

    Wrapped text in UserScreen with cards, added rankingScreen

---
+++ lib/screens/rankingScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RankingScreen extends StatefulWidget {
+  @override
+  _RankingScreenState createState() => _RankingScreenState();
+}
+
+class _RankingScreenState extends State<RankingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
